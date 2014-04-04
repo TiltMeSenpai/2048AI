71bcf35106698f1e8183e8f5c446f85fc32153f9
Joshua Koike <jkoike2013@gmail.com>
2014-04-04 14:35:03 -0700

    Now asks user for an agent if one is not provided as an argument

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import GameAgents
 import inspect
 import argparse
 
+def get_agents():
+    return {i[0] : i[1] for i in inspect.getmembers(GameAgents) if "Agent" in i[0] and not i[0] is "ProtoAgent"}
+
 def main(agent, timeout=1, wait=.1, lookahead_value=.5, depth = 1):
     game = {i[0] : i[1] for i in inspect.getmembers(GameAgents)}[str(agent)](timeout, wait, lookahead_value, depth)
     while True:
@@ -15,10 +18,14 @@ def main(agent, timeout=1, wait=.1, lookahead_value=.5, depth = 1):
 
 if "main" in __name__:
     args = argparse.ArgumentParser(description="AI for 2048 Game")
-    args.add_argument("-agent", type=str, default="StupidAgent")
+    args.add_argument("-agent", type=str, default=None)
     args.add_argument("-t", "--timeout", type=int, default=.5)
     args.add_argument("-w", "--wait",  type=int, default=.1)
     args.add_argument("-l", "--lookahead", type=int, default=.5)
     args.add_argument("-d", "--depth", type=int, default=10)
-    args = vars(args.parse_args())
-    main(args['agent'], args['timeout'], args['wait'], args['lookahead'], args['depth'])
+    args = args.parse_args()
+    if args.agent is None:
+        print("Select an agent by number (0, 1, 2...):")
+        [print(str(i[0]) + ": " + i[1][0]) for i in enumerate(zip(get_agents()))]
+        args.agent = [i for i in get_agents()][int(input(">>>"))]
+    main(args.agent, args.timeout, args.wait, args.lookahead, args.depth)
